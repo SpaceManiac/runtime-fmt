6a975015f1775e82ce00409894e80ce5e1a237b8
Tad Hardesty <tad@platymuus.com>
2017-05-02 00:23:19 -0700

    Add initial implementation of FormatArgs custom derive

---
+++ examples/codegen.rs
@@ -0,0 +1,31 @@
+#[macro_use] extern crate runtime_fmt_derive;
+extern crate runtime_fmt;
+
+use runtime_fmt::PreparedFormat;
+
+#[derive(FormatArgs)]
+struct Struct {
+    left: i32,
+    right: &'static str,
+}
+
+#[derive(FormatArgs)]
+struct TupleStruct(i32, &'static str);
+
+#[derive(FormatArgs)]
+struct UnitStruct;
+
+fn main() {
+    let mut prepared = PreparedFormat::prepare("{left}: {right}").unwrap();
+    prepared.newln();
+    prepared.print(&Struct {
+        left: 42,
+        right: "Hello, world!"
+    });
+
+    PreparedFormat::prepare("{0}: {1}\n").unwrap().print(
+        &TupleStruct(5, "Hello, TupleStruct")
+    );
+
+    PreparedFormat::prepare("Hello, UnitStruct").unwrap().print(&UnitStruct);
+}
--- runtime-fmt-derive/src/lib.rs
+++ runtime-fmt-derive/src/lib.rs
@@ -1,4 +1,6 @@
 //! A custom-derive implementation for the `FormatArgs` trait.
+#![recursion_limit="128"]
+
 extern crate proc_macro;
 extern crate syn;
 #[macro_use] extern crate quote;
@@ -14,10 +16,90 @@ pub fn derive_format_args(input: TokenStream) -> TokenStream {
 }
 
 fn implement(ast: &syn::DeriveInput) -> quote::Tokens {
-    let name = &ast.ident;
+    // The rough structure of this (dummy_ident, extern crate/use) is based on
+    // how serde_derive does it.
+
+    let ident = &ast.ident;
     let variant = match ast.body {
         syn::Body::Struct(ref variant) => variant,
         _ => panic!("#[derive(FormatArgs)] is not implemented for enums")
     };
-    unimplemented!()
+
+    let dummy_ident = syn::Ident::new(format!("_IMPL_FORMAT_ARGS_FOR_{}", ident));
+
+    let (validate_name, validate_index, get_child);
+    match *variant {
+        syn::VariantData::Struct(ref fields) => {
+            get_child = build_fields(fields);
+            validate_index = quote! { false };
+
+            let index = 0..fields.len();
+            let ident: Vec<_> = fields.iter()
+                .map(|field| field.ident.as_ref().unwrap())
+                .map(ToString::to_string)
+                .collect();
+            validate_name = quote! {
+                match name {
+                    #(#ident => _Option::Some(#index),)*
+                    _ => _Option::None,
+                }
+            };
+        }
+        syn::VariantData::Tuple(ref fields) => {
+            get_child = build_fields(fields);
+            validate_name = quote! { _Option::None };
+
+            let len = fields.len();
+            validate_index = quote! { index < #len };
+        }
+        syn::VariantData::Unit => {
+            validate_name = quote! { _Option::None };
+            validate_index = quote! { false };
+            get_child = quote! { panic!("bad index {}", index) };
+        }
+    };
+
+    // TODO: impl_generics, ty_generics, where_clause
+    quote! {
+        #[allow(non_upper_case_globals, unused_attributes)]
+        #[allow(unused_variables, unused_qualifications)]
+        const #dummy_ident: () = {
+            extern crate runtime_fmt as _runtime_fmt;
+            use std::fmt::{Formatter as _Formatter, Result as _Result};
+            use std::option::Option as _Option;
+            #[automatically_derived]
+            impl _runtime_fmt::FormatArgs for #ident {
+                fn validate_name(name: &str) -> _Option<usize> {
+                    #validate_name
+                }
+                fn validate_index(index: usize) -> bool {
+                    #validate_index
+                }
+                fn get_child<__F>(index: usize) -> _Option<fn(&Self, &mut _Formatter) -> _Result>
+                    where __F: _runtime_fmt::codegen::FormatTrait + ?Sized
+                {
+                    #get_child
+                }
+            }
+        };
+    }
+}
+
+fn build_fields(fields: &[syn::Field]) -> quote::Tokens {
+    let index = 0..fields.len();
+    let ty: Vec<_> = fields.iter().map(|field| &field.ty).collect();
+    let ident: Vec<_> = fields.iter().enumerate().map(|(idx, field)| match field.ident {
+        Some(ref ident) => ident.clone(),
+        None => syn::Ident::from(idx),
+    }).collect();
+    quote! {
+        match index {
+            #(
+                #index => _runtime_fmt::codegen::combine::<__F, Self, #ty, _>(
+                    |this| &this.#ident
+                ),
+            )*
+            _ => panic!("bad index {}", index)
+        }
+    }
 }
