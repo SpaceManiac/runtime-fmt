8dabb606455d635061e27c5c6a64bcfad49b24a9
Jacob Smith <jacob101607@yahoo.com>
2017-06-01 13:37:10 -0500

    Implement attributes for renaming and ignoring fields and adding field aliases.

--- examples/codegen.rs
+++ examples/codegen.rs
@@ -24,6 +24,20 @@ struct Alignable {
 #[derive(FormatArgs)]
 struct WithBounds<'a, T: std::fmt::Display + 'a>(&'a T);
 
+#[allow(dead_code)]
+#[derive(FormatArgs)]
+struct WithAttributes {
+    #[format_args(rename = "renamed_field")]
+    field1: &'static str,
+    #[format_args(aliases = "alias")]
+    field2: &'static str,
+    #[format_args(ignore)]
+    ignored: &'static str
+}
+
+#[derive(FormatArgs)]
+struct TupleWithAttributes(#[format_args(rename = "field1")] i32);
+
 fn main() {
     let mut prepared = PreparedFormat::prepare("{left}: {right}").unwrap();
     prepared.newln();
@@ -49,4 +63,24 @@ fn main() {
     });
 
     PreparedFormat::prepare("{}").unwrap().newln().print(&WithBounds(&256));
+
+    let mut prepared = PreparedFormat::prepare(
+r#"WithAttributes {{
+    renamed_field: {renamed_field}
+    field2: {field2}
+    alias: {alias}
+}}"#).unwrap();
+    prepared.newln();
+    prepared.print(&WithAttributes {
+        field1: "field1",
+        field2: "field2",
+        ignored: "ignored"
+    });
+
+    match PreparedFormat::<WithAttributes>::prepare("{ignored}") {
+        Ok(_) => panic!("Field 'ignored' is not ignored"),
+        _ => { }
+    }
+
+    PreparedFormat::prepare("TupleWithAttributes({field1})").unwrap().newln().print(&TupleWithAttributes(256));
 }
